d8d2a79217a518eb2a8746541fcf565df2b0e075
harreand000 <74740246+harreand000@users.noreply.github.com>
2022-09-29 19:33:02 -0400

    Pathing

---
+++ Assets/Scripts/FollowThePath.cs
@@ -0,0 +1,58 @@
+
+using UnityEngine;
+
+public class FollowThePath : MonoBehaviour
+{
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    // Use this for initialization
+    private void Start()
+    {
+
+        // Set position of Enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        // Move Player
+        Move();
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Player didn't reach last waypoint it can move
+        // If player reached last waypoint then it stops
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Player reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Player starts to walk to the next waypoint
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
