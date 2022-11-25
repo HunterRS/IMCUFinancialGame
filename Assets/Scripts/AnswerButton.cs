857ce6edeea8963076591fdb677daa48693be797
HunterRS <hunterrs01@gmail.com>
2022-11-25 15:34:38 -0500

    Answer Response

--- Assets/Scripts/AnswerButton.cs
+++ Assets/Scripts/AnswerButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AnswerButton : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class AnswerButton : MonoBehaviour
     [SerializeField] private GameObject GoodAnswerUI;
     [SerializeField] private GameObject BadAnswerUI;
     [SerializeField] private GameObject ButtonUI;
+    public TextMeshProUGUI greatAnswerResponse;
+    public TextMeshProUGUI goodAnswerResponse;
+    public TextMeshProUGUI badAnswerResponse;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +23,6 @@ public class AnswerButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void AnswerQuestion(int Button)
@@ -30,18 +33,21 @@ public class AnswerButton : MonoBehaviour
             if (GameManager.instance.currentQuestion.answer1movement == 2)
             {
                 Debug.Log("Great Answer");
+                greatAnswerResponse.text = GameManager.instance.currentQuestion.answer1response;
                 GreatAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
             else if (GameManager.instance.currentQuestion.answer1movement == 1)
             {
                 Debug.Log("Good Answer");
+                goodAnswerResponse.text = GameManager.instance.currentQuestion.answer1response;
                 GoodAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
             else if (GameManager.instance.currentQuestion.answer1movement == -2)
             {
                 Debug.Log("bad Answer");
+                badAnswerResponse.text = GameManager.instance.currentQuestion.answer1response;
                 BadAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
@@ -52,18 +58,21 @@ public class AnswerButton : MonoBehaviour
             Debug.Log("Button 2");
             if (GameManager.instance.currentQuestion.answer2movement == 2)
             {
+                greatAnswerResponse.text = GameManager.instance.currentQuestion.answer2response;
                 Debug.Log("Great Answer");
                 GreatAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
             else if (GameManager.instance.currentQuestion.answer2movement == 1)
             {
+                goodAnswerResponse.text = GameManager.instance.currentQuestion.answer2response;
                 Debug.Log("Good Answer");
                 GoodAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
             else if (GameManager.instance.currentQuestion.answer2movement == -2)
             {
+                badAnswerResponse.text = GameManager.instance.currentQuestion.answer2response;
                 Debug.Log("bad Answer");
                 BadAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
@@ -75,18 +84,21 @@ public class AnswerButton : MonoBehaviour
             Debug.Log("Button 3");
             if (GameManager.instance.currentQuestion.answer3movement == 2)
             {
+                greatAnswerResponse.text = GameManager.instance.currentQuestion.answer3response;
                 Debug.Log("Great Answer");
                 GreatAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
             else if (GameManager.instance.currentQuestion.answer3movement == 1)
             {
+                goodAnswerResponse.text = GameManager.instance.currentQuestion.answer3response;
                 Debug.Log("Good Answer");
                 GoodAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
             }
             else if (GameManager.instance.currentQuestion.answer3movement == -2)
             {
+                badAnswerResponse.text = GameManager.instance.currentQuestion.answer3response;
                 Debug.Log("bad Answer");
                 BadAnswerUI.SetActive(true);
                 QuestionUI.SetActive(false);
